c6b750bbcdde654d6774487c0d0904b4a19aad95
Zach Millman <zach.millman@gmail.com>
2024-08-07 10:59:36 -0700

    Polish code formatting

--- app/components/ParcelNumberInput.tsx
+++ app/components/ParcelNumberInput.tsx
@@ -1,7 +1,14 @@
 export interface ParcelNumberInputProps {}
 
+/**
+ * Input for editing a parcel number. 
+ */
 export const ParcelNumberInput: React.FC<ParcelNumberInputProps> = () => {
-    return <input type="text"
+  return (
+    <div>
+      <input type="text"
         placeholder="Parcel number"
         className="h-10 w-full rounded-md border border-input bg-background px-3 py-2"/>
+    </div>
+  );
 }
